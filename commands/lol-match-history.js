7e26f35ffd6d0f9bac3914772363bb0b7c5c2e83
Mark Ganhao <mark.ganhao@gmail.com>
2021-01-17 23:10:01 +0000

    lol match history added

--- commands/lol-match-history.js
+++ commands/lol-match-history.js
@@ -1,4 +1,11 @@
 const fetch = require('node-fetch')
+'use strict';
+const fs = require('fs');
+const { stringify } = require('querystring');
+
+var data = JSON.parse(fs.readFileSync('champions.json'));
+// console.log(Object.values(data.data))
+// Object.keys(rawdata).map(e => console.log(e.id))
 
 module.exports = {
 	
@@ -16,26 +23,63 @@ module.exports = {
 			const summonerName = `/lol/summoner/v4/summoners/by-name`
 			const matchAccId = `/lol/match/v4/matchlists/by-account`
 			const matchId = "/lol/match/v4/matches"
-			const KEY = "RGAPI-763bb50b-3787-42a7-a4c6-49028da13746"
+			const KEY = "RGAPI-34950f9f-5ce0-4bec-a450-62a20a5ad042"
 
 			try {
-				const response = await fetch(
-					`${NAroute}${summonerName}/${summonerQuery}?api_key=${KEY}`
-					)
-						.then( res => res.json())
-						// .then(res => console.log(res))
-						.then( res => fetch(
-							`${NAroute}${matchAccId}/${res.accountId}?api_key=${KEY}`
-						)
-							.then( res => res.json())
-							// .then(res => console.log(res))
-							.then( res => res.matches.slice(0,10))
-							.then(
-								res => Promise.all(res.map(item => fetch(`${NAroute}${matchId}/${item.gameId}?api_key=${KEY}`).then(res => res.json()) ))
-							)
-						)
-					
-				const response2 = response.map(item => item.teams[0].win)
-				message.channel.send(response2);			
+        (async () => {
+          const response1 = await fetch(
+            `${NAroute}${summonerName}/${summonerQuery}?api_key=${KEY}`
+            )
+            .then( res => res.json())
+            .then( res => fetch(
+                `${NAroute}${matchAccId}/${res.accountId}?queue=420&endIndex=10&api_key=${KEY}`
+              ))
+            .then( res => res.json())
+            .catch(error => console.log(error))
+        
+          const response2 = await Promise.all(response1.matches.map(item => 
+            fetch(`${NAroute}${matchId}/${item.gameId}?api_key=${KEY}`)
+            .then(res => res.json()))) 
+        
+          participantIds = []
+          championIds = []
+          const response3 = response2.map((each, index) => {
+            each.participants.reduce(function(arr, e, i) {
+              if (e.championId == response1.matches[index].champion)  {
+                participantIds.push(i+1);
+                championIds.push(e.championId)
+              }
+              return ;
+            }, [])}
+          )
+      
+          let results = []
+          response2.map((each, i) => {
+            if (( each.teams[0].win == "Win" && participantIds[i] <6 ) || ( each.teams[1].win == "Win" && participantIds[i] > 5 ) ) {
+              results.push("Victory")
+            } else { results.push("Defeat") }
+          })
+
+          let finalResults = [];
+          const championNames = []
+          const finalChampions = championIds.map((each, i) => {
+            Object.values(data.data).reduce(function(arr, e, i) {
+              if (e.key == each.toString()) {
+                championNames.push(e.id);
+              } 
+              return ;
+            }, [])
+          })
+
+          championNames.forEach((champion, i) => finalResults.push({[champion] : results[i]}));
+          finalResults = JSON.stringify(finalResults, null, 1)
+          console.log(finalResults[0])
+
+          message.channel.send(finalResults);
+
+        })()			
 			} catch (err) {message.channel.send(err.message);}
 }}};
+
+
+
